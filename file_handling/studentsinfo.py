0e4cfbdaae79e81fd49bcd00a3ace7ea82ec335b
rijan808-cyber <rijanpariyar808@gmail.com>
2026-06-30 13:41:00 +0545

    Updated code for filehandling

--- file_handling/studentsinfo.py
+++ file_handling/studentsinfo.py
@@ -1,13 +1,13 @@
 def storestudentsdetails():
-    i=1
-    f=open('studentsdetails.txt','a')
+    i = 1
+    f = open('studentsdetails.txt','a')
     while True:
-        name=input(f"Enter Student {i} name: ")
-        age=int(input(f"Enter Student {i} age: "))
-        id=int(input(f"Enter Student {i} id: "))
-        email=input(f"Enter Student {i} email: ")
-        contact=int(input(f"Enter Student {i} contact: "))
-        fees=float(input(f"Enter Student {i} fees: "))
+        name = input(f"Enter Student {i} name: ")
+        age = int(input(f"Enter Student {i} age: "))
+        id = int(input(f"Enter Student {i} id: "))
+        email = input(f"Enter Student {i} email: ")
+        contact = int(input(f"Enter Student {i} contact: "))
+        fees = float(input(f"Enter Student {i} fees: "))
         print()
 
         f.write(f'{id} | {name} | {age} | {email} | {contact} | {fees} \n')
@@ -18,9 +18,30 @@ def storestudentsdetails():
         if option.lower() == 'no':
                 break
     f.close()
-    
+
 def readstudentdetails():
-    f=open('studentsdetails.txt')
+    f = open('studentsdetails.txt')
     data = f.readlines()
     for line in data:
         print(line, end='')
+    f.close()
+
+def search_by_id():
+    f = open('studentsdetails.txt')
+    data = f.readlines()
+    ids=int(input("Enter Students ID for search: "))
+    for line in data:
+        id, name, age, email, contact, fees = line.split('|')
+        if int(id) == ids:
+            print(f'Student Name: {name}')
+            print(f'Student ID: {id}')
+            print(f'Student Age: {age}')
+            print(f'Student Contact: {contact}')
+            print(f'Student Email: {email}')
+            print(f'Student Fees: {fees}')
+            break
+    else:
+        print("Student Not Found")
+    f.close()
+
+search_by_id()
---
+++ file_handling/withkey.py
@@ -0,0 +1,4 @@
+with open ('abc.txt','a') as f:
+    print(f'is File closed: {f.closed}')
+
+print(f"Is file closed: {f.closed}")
