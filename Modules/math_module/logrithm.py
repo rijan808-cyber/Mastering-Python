7b2065e7c1143d73eb329ed27d94c93944cfc247
rijan808-cyber <rijanpariyar808@gmail.com>
2026-07-07 13:23:51 +0545

    math module complete

---
+++ Modules/math_module/logrithm.py
@@ -0,0 +1,6 @@
+import math
+print(f"Natural log: {math.log(5)}")
+print(f"Log base 10: {math.log10(5)}")
+print(f"Log base 2: {math.log2(5)}")
+print(f"e power of x: {math.exp(1.6094379124341003)}")
+print(f"e power of x-1: {math.expm1(1.6094379124341003)}")
---
+++ Modules/math_module/per_com.py
@@ -0,0 +1,6 @@
+import math 
+print(f"Permutation where order matter: {math.perm(4,2)}")
+print(f"Combination where order doesn't matter: {math.comb(4,2)}")
+print(f"Factorial of 4: {math.factorial(4)}")
+print(f"GCD of 12 and 18: {math.gcd(12,18)}")
+print(f"LCM of 12 and 18: {math.lcm(4)}")
